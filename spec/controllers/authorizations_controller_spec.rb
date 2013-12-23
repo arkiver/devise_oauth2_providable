e6fe21e9ee1a960f04bea3b8023d51b2e6b34dce
Akshay Khole <akshay@idyllic-software.com>
2013-12-23 19:17:53 +0530

    Fixing auth controller specs. Pending a few

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -1,3 +1,4 @@
+# Deprecations here: http://robots.thoughtbot.com/shoulda-matchers-2-0
 require 'spec_helper'
 
 describe Devise::Oauth2Providable::AuthorizationsController do
@@ -9,11 +10,11 @@ describe Devise::Oauth2Providable::AuthorizationsController do
       before do
         sign_in user
         get :new, :client_id => client.identifier, :redirect_uri => redirect_uri, :response_type => 'code', :use_route => 'devise_oauth2_providable'
+        response.content_type.should eq 'text/html'
       end
       it { should respond_with :ok }
-      it { should respond_with_content_type :html }
-      it { should assign_to(:redirect_uri).with(redirect_uri) }
-      it { should assign_to(:response_type) }
+      pending { should assign_to(:redirect_uri).with(redirect_uri) }
+      pending { should assign_to(:response_type) }
       it { should render_template 'devise/oauth2_providable/authorizations/new' }
       it { should render_with_layout 'application' }
     end
@@ -24,9 +25,9 @@ describe Devise::Oauth2Providable::AuthorizationsController do
       before do
         sign_in user
         get :new, :client_id => client.identifier, :redirect_uri => redirect_uri, :response_type => 'code', :use_route => 'devise_oauth2_providable'
+        response.content_type.should eq 'text/html'
       end
       it { should respond_with :bad_request }
-      it { should respond_with_content_type :html }
     end
     context "with ssl enforced" do
       with :user
